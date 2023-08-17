3ddf0aa69634676887198978152d6a531049eb96
Carlos Palhares <chjunior@gmail.com>
2023-08-17 15:53:56 -0300

    Add context update api

--- audiences/app/controllers/audiences/contexts_controller.rb
+++ audiences/app/controllers/audiences/contexts_controller.rb
@@ -6,10 +6,18 @@ module Audiences
       render_context Audiences.load(params[:key])
     end
 
+    def update
+      render_context Audiences.update(params[:key], context_params)
+    end
+
   private
 
     def render_context(context)
       render json: context.as_json(only: %w[match_all], methods: %w[criteria])
+    end
+
+    def context_params
+      params.slice(:match_all).permit!
     end
   end
 end
--- audiences/spec/requests/contexts_api_spec.rb
+++ audiences/spec/requests/contexts_api_spec.rb
@@ -3,22 +3,29 @@
 require "rails_helper"
 
 RSpec.describe "/audiences", type: :request do
-  let(:parsed_body) { JSON.parse(last_response.body) }
   let(:example_owner) { ExampleOwner.create!(name: "Example Owner") }
 
   context "GET /audiences/:context_key" do
     it "responds with the audience context json" do
       get audience_context_path(example_owner)
 
-      expect(parsed_body).to match({ "match_all" => false, "criteria" => [] })
+      expect(response.parsed_body).to match({ "match_all" => false, "criteria" => [] })
+    end
+  end
+
+  context "PUT /audiences/:context_key" do
+    it "updates the audience context" do
+      put audience_context_path(example_owner), params: { match_all: true }
+
+      context = Audiences::Context.for(example_owner)
+
+      expect(context).to be_match_all
     end
 
-    it "responds with a valid context context_key" do
-      get audience_context_path(example_owner), format: :json
+    it "responds with the audience context json" do
+      put audience_context_path(example_owner), params: { match_all: true }
 
-      audience = Audiences.load(parsed_body["key"])
-
-      expect(audience.owner).to eql example_owner
+      expect(response.parsed_body).to match({ "match_all" => true, "criteria" => [] })
     end
   end
 end
