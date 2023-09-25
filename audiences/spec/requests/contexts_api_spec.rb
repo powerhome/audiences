678e1aaff30de67b34fc431a195e6ea05409dc2c
Carlos Palhares <chjunior@gmail.com>
2023-09-25 15:47:46 -0300

    Fix undesired module

--- audiences/spec/requests/contexts_api_spec.rb
+++ audiences/spec/requests/contexts_api_spec.rb
@@ -3,20 +3,19 @@
 require "rails_helper"
 
 RSpec.describe "/audiences", type: :request do
-  let(:parsed_body) { JSON.parse(last_response.body) }
   let(:example_owner) { ExampleOwner.create!(name: "Example Owner") }
 
   context "GET /audiences/:context_key" do
     it "responds with the audience context json" do
       get audience_context_path(example_owner)
 
-      expect(parsed_body).to match({ "match_all" => false, "criteria" => [] })
+      expect(response.parsed_body).to match({ "match_all" => false, "criteria" => [] })
     end
   end
 
   context "PUT /audiences/:context_key" do
     it "updates the audience context" do
-      put audience_context_path(example_owner), match_all: true
+      put audience_context_path(example_owner), params: { match_all: true }
 
       context = Audiences::Context.for(example_owner)
 
@@ -24,9 +23,9 @@ RSpec.describe "/audiences", type: :request do
     end
 
     it "responds with the audience context json" do
-      put audience_context_path(example_owner), match_all: true
+      put audience_context_path(example_owner), params: { match_all: true }
 
-      expect(parsed_body).to match({ "match_all" => true, "criteria" => [] })
+      expect(response.parsed_body).to match({ "match_all" => true, "criteria" => [] })
     end
   end
 end
