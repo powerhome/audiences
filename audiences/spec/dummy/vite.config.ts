16718f339a683531cfbf081ad65d9c2b3fc3a4cb
Guilherme Burille Moretti <guilherme.moretti@powerhrg.com>
2023-11-16 13:25:42 -0300

    [HFH-1731] Fix hot module reload on dummy app (#72)

--- audiences/spec/dummy/vite.config.ts
+++ audiences/spec/dummy/vite.config.ts
@@ -1,7 +1,13 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import RubyPlugin from "vite-plugin-ruby";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [RubyPlugin(), react()],
+  resolve: {
+    alias: {
+      "audiences": resolve(__dirname, "../../../audiences-react/src")
+    }
+  }
 });
