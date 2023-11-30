0f898adea83ce7e5f06422e9f6fc73e2db99538d
Carlos Palhares <chjunior@gmail.com>
2023-11-30 10:57:26 -0300

    Fix stub membership job

--- audiences/spec/dummy/app/jobs/update_memberships_job.rb
+++ audiences/spec/dummy/app/jobs/update_memberships_job.rb
@@ -2,15 +2,13 @@
 
 class UpdateMembershipsJob < ApplicationJob
   def perform(context)
-    memberships = owner.memberships.build(
-      context.users.map do |user|
-        {
-          user_id: user["id"],
-          name: user["displayName"],
-          photo: user.dig("photos", 0, "value"),
-        }
-      end
-    )
+    memberships = context.users.map do |user|
+      ExampleMembership.new(
+        user_id: user["id"],
+        name: user["displayName"],
+        photo: user.dig("photos", 0, "value")
+      )
+    end
     context.owner.update!(memberships: memberships)
   end
 end
