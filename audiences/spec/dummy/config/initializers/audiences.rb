99449e6e6862b60f1762993e3de138262891c2b0
Carlos Palhares <chjunior@gmail.com>
2024-07-18 17:15:27 -0300

    Update initializer to new configuration

--- audiences/spec/dummy/config/initializers/audiences.rb
+++ audiences/spec/dummy/config/initializers/audiences.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-Audiences::Scim.client = Audiences::Scim::Client.new(
-  uri: ENV.fetch("SCIM_V2_API", "http://example.com/scim/v2/"),
-  headers: { "Authorization" => ENV.fetch("SCIM_AUTHORIZATION", "Bearer 123456789") }
-)
+Audiences.configure do |config|
+  config.scim = {
+    uri: ENV.fetch("SCIM_V2_API", "http://example.com/scim/v2/"),
+    headers: { "Authorization" => ENV.fetch("SCIM_AUTHORIZATION", "Bearer 123456789") }
+  }
 
-Audiences::Scim.defaults[:Users] = { attributes: "id,externalId,displayName,photos" }
-
-Rails.application.config.to_prepare do
-  Audiences::Notifications.subscribe ExampleOwner, job: UpdateMembershipsJob
+  config.subscriptions do
+    subscribe ExampleOwner, job: UpdateMembershipsJob
+  end
 end
