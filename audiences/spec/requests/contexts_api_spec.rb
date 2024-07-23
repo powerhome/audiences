5bb412b55b9d38d89b0236420aa15ca6c7ff7ae8
Carlos Palhares <chjunior@gmail.com>
2024-07-22 22:20:35 -0300

    Update contexts api spec to use autobuilt context relation

--- audiences/spec/requests/contexts_api_spec.rb
+++ audiences/spec/requests/contexts_api_spec.rb
@@ -4,11 +4,10 @@ require "rails_helper"
 
 RSpec.describe "/audiences" do
   let(:example_owner) { ExampleOwner.create!(name: "Example Owner") }
-  let(:context_key) { Audiences::Context.for(example_owner).signed_key }
 
   describe "GET /audiences/:context_key" do
     it "responds with the audience context json" do
-      get audiences.signed_context_path(context_key)
+      get audience_context_path(example_owner, :members)
 
       expect(response.parsed_body).to match({
                                               "match_all" => false,
@@ -30,21 +29,22 @@ RSpec.describe "/audiences" do
       stub_request(:get, "http://example.com/scim/v2/Users?attributes=id,externalId,displayName,photos")
         .to_return(status: 200, body: users_response.to_json, headers: {})
 
-      put audiences.signed_context_path(context_key), as: :json, params: { match_all: true }
+      put audience_context_path(example_owner, :members), as: :json, params: { match_all: true }
 
-      context = Audiences::Context.for(example_owner)
+      context = example_owner.members_context.reload
 
       expect(context).to be_match_all
       expect(context.users.count).to eql 2
     end
 
     it "updates the context extra users" do
-      put audiences.signed_context_path(context_key),
+      put audience_context_path(example_owner, :members),
           as: :json,
           params: { extra_users: [{ externalId: 123, displayName: "John Doe",
                                     photos: [{ value: "http://example.com" }] }] }
 
-      context = Audiences::Context.for(example_owner)
+      context = example_owner.members_context.reload
+
       expect(context.extra_users).to eql [{
         "externalId" => 123,
         "displayName" => "John Doe",
@@ -53,7 +53,7 @@ RSpec.describe "/audiences" do
     end
 
     it "responds with the audience context json" do
-      put audiences.signed_context_path(context_key),
+      put audience_context_path(example_owner, :members),
           as: :json,
           params: { extra_users: [{ externalId: 123, displayName: "John Doe",
                                     photos: [{ value: "http://example.com" }] }] }
@@ -91,7 +91,7 @@ RSpec.describe "/audiences" do
                            "&filter=groups.value eq 987")
           .to_return(status: 200, body: users_response.to_json, headers: {})
 
-        put audiences.signed_context_path(context_key),
+        put audience_context_path(example_owner, :members),
             as: :json,
             params: {
               match_all: false,
@@ -133,12 +133,13 @@ RSpec.describe "/audiences" do
 
   describe "GET /audiences/:context_key/users" do
     it "is the list of users from an audience context" do
-      context = Audiences::Context.for(example_owner)
-      context.users.create(user_id: 123, data: { "externalId" => 123 })
-      context.users.create(user_id: 456, data: { "externalId" => 456 })
-      context.users.create(user_id: 789, data: { "externalId" => 789 })
+      example_owner.members_context.users.create([
+                                                   { user_id: 123, data: { "externalId" => 123 } },
+                                                   { user_id: 456, data: { "externalId" => 456 } },
+                                                   { user_id: 789, data: { "externalId" => 789 } },
+                                                 ])
 
-      get audiences.users_path(context_key)
+      get audiences.users_path(example_owner.members_context.signed_key)
 
       expect(response.parsed_body).to match({
                                               "count" => 3,
@@ -153,13 +154,20 @@ RSpec.describe "/audiences" do
 
   describe "GET /audiences/:context_key/users/:criterion_id" do
     it "is the list of users from an audience context's criterion" do
-      context = Audiences::Context.for(example_owner)
-      criterion = context.criteria.create!
-      criterion.users.create(user_id: 1, data: { "externalId" => 1, "displayName" => "John" })
-      criterion.users.create(user_id: 2, data: { "externalId" => 2, "displayName" => "Jose" })
-      criterion.users.create(user_id: 3, data: { "externalId" => 3, "displayName" => "Nelson" })
+      criterion = example_owner.members_context.criteria.create!
+      criterion.users.create!([
+                                { user_id: 1,
+                                  data: { "externalId" => 1,
+                                          "displayName" => "John" } },
+                                { user_id: 2,
+                                  data: { "externalId" => 2,
+                                          "displayName" => "Jose" } },
+                                { user_id: 3,
+                                  data: { "externalId" => 3,
+                                          "displayName" => "Nelson" } },
+                              ])
 
-      get audiences.users_path(context_key, criterion_id: criterion.id)
+      get audiences.users_path(example_owner.members_context.signed_key, criterion_id: criterion.id)
 
       expect(response.parsed_body).to match_array({
                                                     "count" => 3,
