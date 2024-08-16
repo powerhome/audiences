1f109538676e1d12f66e428d58bf8cdf2d32675e
Carlos Palhares <chjunior@gmail.com>
2024-08-16 16:44:23 -0300

    Strip schemas and meta from SCIM proxy

--- audiences/spec/requests/scim_proxy_spec.rb
+++ audiences/spec/requests/scim_proxy_spec.rb
@@ -5,9 +5,21 @@ require "rails_helper"
 RSpec.describe "/audiences/scim" do
   let(:resources) do
     [
-      { externalId: "1", displayName: "A Name", photos: "photo 1" },
-      { externalId: "2", displayName: "Another Name", photos: "photo 2" },
-      { externalId: "3", displayName: "YAN", photos: "photo 3" },
+      {
+        schemas: ["schema:a", "schema:b"],
+        meta: { location: "http://example.com/scim/v2/MyResources/1" },
+        externalId: "1", displayName: "A Name", photos: "photo 1"
+      },
+      {
+        schemas: ["schema:a", "schema:b"],
+        meta: { location: "http://example.com/scim/v2/MyResources/2" },
+        externalId: "2", displayName: "Another Name", photos: "photo 2"
+      },
+      {
+        schemas: ["schema:a", "schema:b"],
+        meta: { location: "http://example.com/scim/v2/MyResources/3" },
+        externalId: "3", displayName: "YAN", photos: "photo 3"
+      },
     ].as_json
   end
   let(:response_body) do
@@ -28,6 +40,17 @@ RSpec.describe "/audiences/scim" do
                                                 "photos" => "photo 2" },
                                               { "displayName" => "YAN", "externalId" => "3", "photos" => "photo 3" },
                                             ])
+    end
+
+    it "removes the schemas and meta from the resources" do
+      attrs = "id,externalId,displayName"
+      stub_request(:get, "http://example.com/scim/v2/MyResources?attributes=#{attrs}&filter=name eq John")
+        .to_return(status: 200, body: response_body, headers: {})
+
+      get audience_scim_proxy_path(scim_path: "MyResources", filter: "name eq John")
+
+      expect(response.parsed_body.pluck("schemas").compact).to be_empty
+      expect(response.parsed_body.pluck("meta").compact).to be_empty
     end
   end
 end
