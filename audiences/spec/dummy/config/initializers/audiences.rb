9b7b6ffbda435ed1a45f4236c548940a4de04bb7
Carlos Palhares <chjunior@gmail.com>
2024-10-31 14:53:12 +0000

    Authenticate Audiences requests

--- audiences/spec/dummy/config/initializers/audiences.rb
+++ audiences/spec/dummy/config/initializers/audiences.rb
@@ -3,6 +3,8 @@
 Audiences.configure do |config|
   config.identity_class = "ExampleUser"
 
+  config.authenticate = ->(*) { true }
+
   config.scim = {
     uri: ENV.fetch("SCIM_V2_API", "http://example.com/scim/v2/"),
     headers: { "Authorization" => ENV.fetch("SCIM_AUTHORIZATION", "Bearer 123456789") },
