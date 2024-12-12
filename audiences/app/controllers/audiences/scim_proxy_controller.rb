b662947bbb54833820e5ac56b7df5883b7ea82c0
Carlos Palhares <chjunior@gmail.com>
2024-12-12 12:03:37 -0300

    Protect SCIM data (#451)
    Audiences currently exposes all default attributes of a resource through
    the Proxy, including sensitive information. This PR limits the data and
    filters on the Audiences proxy to what is necessary for the UI:
    
    - **Limit data to what the UI need**
    - **Only filter by displayName containing filter string**

--- audiences/app/controllers/audiences/scim_proxy_controller.rb
+++ audiences/app/controllers/audiences/scim_proxy_controller.rb
@@ -4,7 +4,11 @@ module Audiences
   class ScimProxyController < ApplicationController
     def get
       resources = Audiences::Scim.resource(params[:scim_path].to_sym)
-                                 .query(filter: params[:filter], startIndex: params[:startIndex], count: params[:count])
+                                 .query(
+                                   filter: "displayName co \"#{params[:filter]}\"",
+                                   startIndex: params[:startIndex], count: params[:count],
+                                   attributes: %w[id externalId displayName photos]
+                                 )
 
       render json: resources, except: %w[schemas meta]
     end
