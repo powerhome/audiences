2246d7ec84cec14dc5ce8ca591e52682d54cad56
Carlos Palhares <chjunior@gmail.com>
2024-12-18 18:51:25 -0300

    Remove unused routes

--- audiences/config/routes.rb
+++ audiences/config/routes.rb
@@ -8,14 +8,3 @@ Audiences::Engine.routes.draw do
   get "/:key/users(/:criterion_id)" => "contexts#users", as: :users
   put "/:key" => "contexts#update"
 end
-
-Rails.application.routes.draw do
-  direct :audience_context do |owner, relation = nil|
-    context = Audiences::Context.for(owner, relation: relation)
-    audiences.route_for(:signed_context, key: context.signed_key, **url_options)
-  end
-
-  direct :audience_scim_proxy do |options|
-    audiences.route_for(:scim_proxy, **url_options, **options)
-  end
-end
