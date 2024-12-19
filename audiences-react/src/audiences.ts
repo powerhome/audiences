2c18b35c70dac50548441bed0f58ddf9538e8e39
Carlos Palhares <chjunior@gmail.com>
2024-12-19 14:43:51 -0300

    Replace use-http by much smaller and better useFetch

--- audiences-react/src/audiences.ts
+++ audiences-react/src/audiences.ts
@@ -1,10 +1,10 @@
-import { createContext, useContext, useEffect } from "react"
-import useFetch, { CachePolicies, IncomingOptions } from "use-http"
+import { createContext, useContext, useEffect, useState } from "react"
 
 import useFormReducer, {
   RegistryAction,
   UseFormReducer,
 } from "./useFormReducer"
+import { useFetch } from "./useFetch"
 import { AudienceContext, GroupCriterion, ScimObject } from "./types"
 import { set } from "lodash/fp"
 
@@ -32,13 +32,10 @@ export type UseAudienceContext = UseFormReducer<AudienceContext> & {
 export function useAudiences(
   uri: string,
   key: string,
-  options: IncomingOptions = {},
+  options: RequestInit = {},
 ): UseAudienceContext {
-  const { put, response, loading: saving } = useFetch(uri, options)
-  const { get } = useFetch(uri, {
-    ...options,
-    cachePolicy: CachePolicies.NO_CACHE,
-  })
+  const [ saving, setSaving ] = useState(false)
+  const { get, put } = useFetch(uri, options)
   const criteriaForm = useFormReducer<AudienceContext>({} as AudienceContext, {
     "remove-criteria": (
       context,
@@ -73,21 +70,20 @@ export function useAudiences(
     offset?: number,
   ) {
     return get(
-      `/${key}/users/${criterion?.id || ""}?offset=${offset}&search=${search}`,
+      `${key}/users/${criterion?.id || ""}?offset=${offset}&search=${search}`,
     )
   }
 
   async function query(resourceId: string, displayName: string) {
-    return await get(`/scim/${resourceId}?filter=${displayName}`)
+    return await get(`scim/${resourceId}?filter=${displayName}`)
   }
 
   async function save() {
-    const updatedContext = await put(key, criteriaForm.value)
-    if (response.ok) {
-      criteriaForm.reset(updatedContext)
-    } else {
-      criteriaForm.setError("Unhandled server error")
-    }
+    setSaving(true)
+    return put(key, criteriaForm.value)
+        .then((response: AudienceContext) => criteriaForm.reset(response))
+        .catch((error: Error) => criteriaForm.setError(error.message))
+        .finally(() => setSaving(false))
   }
 
   return {
