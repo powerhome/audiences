b4957bb1bfafc6ecdce813fda28df4c333f460c9
Kseniia Khodyreva <melidiosone@gmail.com>
2025-03-05 16:29:32 -0500

    Fixes

--- audiences-react/vite.config.ts
+++ audiences-react/vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
+import path from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import dts from "vite-plugin-dts"
 
 export default defineConfig({
   plugins: [
@@ -21,4 +21,4 @@ export default defineConfig({
       external: ["react", "react-dom", "playbook-ui", "react/jsx-runtime"],
     },
   },
-});
+})
