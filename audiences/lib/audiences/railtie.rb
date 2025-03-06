4155e7e578833d7935fc4cfb80f638cfe32610d6
Kseniia Khodyreva <melidiosone@gmail.com>
2025-03-06 16:00:41 -0500

    Updated railtie

--- audiences/lib/audiences/railtie.rb
+++ audiences/lib/audiences/railtie.rb
@@ -4,6 +4,7 @@ module Audiences
   class Railtie < Rails::Railtie
     initializer "audiences.editor_helper" do
       ActiveSupport.on_load(:action_view) do
+        require "audiences/editor_helper"
         include Audiences::EditorHelper
       end
     end
