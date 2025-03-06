c3da58a3d1b2ab1d5cc38add24cc7fea8b9c823f
Kseniia Khodyreva <melidiosone@gmail.com>
2025-03-06 10:58:34 -0500

    Turbo handling

--- audiences/lib/audiences/audiences.js
+++ audiences/lib/audiences/audiences.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AudiencesEditor from "audiences-react";
 
-document.addEventListener("DOMContentLoaded", () => {
+function mountAudiencesEditors() {
   document.querySelectorAll("[data-react-class='AudiencesEditor']").forEach((element) => {
-    const uri = element.dataset.audiencesUri;
-    const context = element.dataset.audiencesContext;
-    ReactDOM.render(<AudiencesEditor uri={uri} context={context} />, element);
+    if (!element.dataset.reactMounted) {
+      const uri = element.dataset.audiencesUri;
+      const context = element.dataset.audiencesContext;
+      ReactDOM.render(<AudiencesEditor uri={uri} context={context} />, element);
+      element.dataset.reactMounted = "true";
+    }
   });
-});
+}
+
+document.addEventListener("DOMContentLoaded", mountAudiencesEditors);
+document.addEventListener("turbo:load", mountAudiencesEditors);
+document.addEventListener("turbo:render", mountAudiencesEditors);
