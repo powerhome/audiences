9d0c87b63953611a98fcbe2a467da3dcc14e3412
Carlos Palhares <carlos.palhares@powerhrg.com>
2025-05-21 17:10:59 +0000

    Add spec covering update on create

--- audiences/spec/observers/audiences/two_percent/groups_observer_spec.rb
+++ audiences/spec/observers/audiences/two_percent/groups_observer_spec.rb
@@ -20,6 +20,23 @@ RSpec.describe Audiences::TwoPercent::GroupsObserver do
     expect(created_group.data).to eql params
   end
 
+  it "updates a group that is configured in Audiences.config.group_types even with CreateEvent" do
+    params = { "displayName" => "My Group", "externalId" => "external-id-123" }
+    group = Audiences::Group.create(resource_type: "Groups", display_name: "Old Group Name",
+                                    external_id: "external-id-123")
+
+    expect do
+      TwoPercent::CreateEvent.create(resource: "Groups", params: params)
+    end.to_not(change { Audiences::Group.count })
+
+    group.reload
+
+    expect(group.resource_type).to eql "Groups"
+    expect(group.display_name).to eql "My Group"
+    expect(group.external_id).to eql "external-id-123"
+    expect(group.data).to eql params
+  end
+
   it "updates a group that is configured in Audiences.config.group_types" do
     params = { "displayName" => "My Group", "externalId" => "external-id-123" }
     group = Audiences::Group.create(resource_type: "Groups", display_name: "Old Group Name",
