8cdf7f3aba0ed5f747575c58bfb16d3d1bad2e3b
Carlos Palhares <carlos.palhares@powerhrg.com>
2025-05-28 17:22:20 -0300

    Update migration to populate display_name and picture_urls

--- audiences/db/migrate/20250527203948_populate_external_users_scim_id.rb
+++ audiences/db/migrate/20250527203948_populate_external_users_scim_id.rb
@@ -5,7 +5,11 @@ class PopulateExternalUsersScimId < ActiveRecord::Migration[6.1]
 
   def up
     Audiences::ExternalUser.find_each do |user|
-      user.update!(scim_id: user.data&.fetch("id", nil))
+      user.update!(
+        scim_id: user.data&.fetch("id", nil),
+        display_name: user.data&.fetch("displayName", nil),
+        picture_urls: user.data&.fetch("photos", [])&.pluck("value"),
+      )
     end
   end
 end
