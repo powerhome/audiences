3c21bf41811b73fbe3b9412b075ab31c8d7203c3
Carlos Palhares <carlos.palhares@powerhrg.com>
2025-06-17 17:21:52 +0000

    Remove unused methods in ExternalUser

--- audiences/app/models/audiences/external_user.rb
+++ audiences/app/models/audiences/external_user.rb
@@ -37,27 +37,6 @@ module Audiences
       self.picture_url = urls&.first
     end
 
-    def self.fetch(external_ids, count: 100)
-      return [] unless external_ids.any?
-
-      Array(external_ids).in_groups_of(count, false).flat_map do |ids|
-        filter = Array(ids).map { "externalId eq #{_1}" }.join(" OR ")
-        Audiences::Scim.resource(:Users).all(count: count, filter: filter).to_a
-      end
-    end
-
-    def self.wrap(resources)
-      return [] unless resources&.any?
-
-      attrs = resources.map do |data|
-        { scim_id: data["id"], user_id: data["externalId"], data: data, created_at: Time.current,
-          updated_at: Time.current }
-      end
-      unique_by = :scim_id if connection.supports_insert_conflict_target?
-      upsert_all(attrs, unique_by: unique_by) # rubocop:disable Rails/SkipsModelValidations
-      where(scim_id: attrs.pluck(:scim_id))
-    end
-
     def as_json(...)
       data.as_json(...)
     end
