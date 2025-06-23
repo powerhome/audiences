e0d75d61bab451715973831607cff32b9db8918e
Carlos Palhares <carlos.palhares@powerhrg.com>
2025-06-23 13:16:06 +0000

    Migrate criterion_json to group criterion

--- audiences/spec/dummy/db/schema.rb
+++ audiences/spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_20_005800) do
+ActiveRecord::Schema.define(version: 2025_06_23_131202) do
 
   create_table "audiences_contexts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "owner_type", null: false
