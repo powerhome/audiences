10403d15015b64c350fe86b1d7640c6d074fccaf
Carlos Palhares <carlos.palhares@powerhrg.com>
2025-06-23 23:41:11 +0000

    Fix migration

--- audiences/db/migrate/20250623131202_move_group_criterion_to_criterion_groups.rb
+++ audiences/db/migrate/20250623131202_move_group_criterion_to_criterion_groups.rb
@@ -3,10 +3,10 @@
 class MoveGroupCriterionToCriterionGroups < ActiveRecord::Migration[6.1]
   def up
     Audiences::Criterion.find_each do |criterion|
-      next if criterion.groups_json.empty?
+      next if criterion.groups_json.blank?
 
       groups = criterion.groups_json.values.flat_map do |scim_groups|
-        scim_groups.pluck(:id)
+        scim_groups.pluck("id")
       end
 
       criterion.update!(groups: Audiences::Group.where(scim_id: groups))
