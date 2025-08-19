58804b93740d30dca77706fa7627b4f254556ea4
Carlos Palhares <carlos.palhares@powerhrg.com>
2025-08-19 16:27:25 +0000

    Remove SCIM configuration and documentation

--- audiences/spec/dummy/config/initializers/audiences.rb
+++ audiences/spec/dummy/config/initializers/audiences.rb
@@ -8,11 +8,6 @@ Audiences.configure do |config|
 
   config.authenticate = ->(*) { true }
 
-  config.scim = {
-    uri: ENV.fetch("SCIM_V2_API", "http://example.com/scim/v2/"),
-    headers: { "Authorization" => ENV.fetch("SCIM_AUTHORIZATION", "Bearer 123456789") },
-  }
-
   config.notifications do
     subscribe ExampleOwner, job: UpdateMembershipsJob
   end
