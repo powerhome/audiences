1f85dfee4979528ef3da2438a4e6b7d0bb8dd2e7
Carlos Palhares <carlos.palhares@powerhrg.com>
2025-08-23 21:03:48 -0300

    Make MatchAllToggleHeader just a header again

--- audiences-react/src/AudienceForm/MatchAllToggleHeader.tsx
+++ audiences-react/src/AudienceForm/MatchAllToggleHeader.tsx
@@ -1,61 +1,49 @@
 import { FlexItem, Toggle } from "playbook-ui"
-import { ReactNode } from "react"
-import { Card, Caption, Flex } from "playbook-ui"
+import { Caption, Flex } from "playbook-ui"
 
 import { MembersModalButton } from "./MembersModalButton"
 import { useAudiencesContext } from "../audiences"
 
-type MatchAllToggleCardProps = {
-  children: ReactNode
+type MatchAllToggleHeaderProps = {
   allowMatchAll: boolean
 }
-export function MatchAllToggleCard({ children, allowMatchAll }: MatchAllToggleCardProps) {
+export function MatchAllToggleHeader({ allowMatchAll }: MatchAllToggleHeaderProps) {
   const { value: context, isDirty, change } = useAudiencesContext()
   const handleToggle = () => change("match_all", !context.match_all)
 
   return (
-    <Card margin="xs" padding="xs">
-      <Card.Header headerColor={context.match_all ? "none" : "white"}>
-        <Flex orientation="row" spacing="between" wrap>
-          <FlexItem>
-            <Caption text={`Members ${isDirty() ? "" : context.count}`} />
-            {isDirty() ? (
-              <Caption
-                size="xs"
-                marginTop="sm"
-                text="Audience members will update when the page is saved"
+    <Flex orientation="row" spacing="between" wrap>
+      <FlexItem>
+        <Caption text={`Members ${isDirty() ? "" : context.count}`} />
+        {isDirty() ? (
+          <Caption
+            size="xs"
+            marginTop="sm"
+            text="Audience members will update when the page is saved"
+          />
+        ) : (
+          <MembersModalButton
+            text="View All"
+            title="All Members"
+            padding="none"
+            total={context.count}
+          />
+        )}
+      </FlexItem>
+      {allowMatchAll && (
+        <FlexItem>
+          <Flex justify="right" orientation="row" align="center">
+            <Toggle>
+              <input
+                type="checkbox"
+                checked={context.match_all}
+                onChange={handleToggle}
               />
-            ) : (
-              <MembersModalButton
-                text="View All"
-                title="All Members"
-                padding="none"
-                total={context.count}
-              />
-            )}
-          </FlexItem>
-          {allowMatchAll && (
-            <FlexItem>
-              <Flex justify="right" orientation="row" align="center">
-                <Toggle>
-                  <input
-                    type="checkbox"
-                    checked={context.match_all}
-                    onChange={handleToggle}
-                  />
-                </Toggle>
-                <Caption marginLeft="xs" size="xs" text={"All Employees"} />
-              </Flex>
-            </FlexItem>
-          )}
-        </Flex>
-      </Card.Header>
-
-      <Card.Body>
-        <Flex orientation="column" align="stretch">
-          {children}
-        </Flex>
-      </Card.Body>
-    </Card>
+            </Toggle>
+            <Caption marginLeft="xs" size="xs" text={"All Employees"} />
+          </Flex>
+        </FlexItem>
+      )}
+    </Flex>
   )
 }
--- audiences-react/src/AudienceForm/index.tsx
+++ audiences-react/src/AudienceForm/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { FixedConfirmationToast, Button, FlexItem } from "playbook-ui"
+import { FixedConfirmationToast, Button, Flex, Card, FlexItem } from "playbook-ui"
 import { isEmpty } from "lodash/fp"
 
 import { ScimObject } from "../types"
@@ -9,7 +9,7 @@ import { ScimResourceTypeahead } from "./ScimResourceTypeahead"
 import { CriteriaList } from "./CriteriaList"
 import { CriteriaForm } from "./CriteriaForm"
 import { ActionBar } from "./ActionBar"
-import { MatchAllToggleCard } from "./MatchAllToggleHeader"
+import { MatchAllToggleHeader } from "./MatchAllToggleHeader"
 import { useAudiencesContext } from "../audiences"
 
 type AudienceFormProps = {
@@ -43,34 +43,41 @@ export const AudienceForm = ({
   }
 
   return (
-    <MatchAllToggleCard allowMatchAll={allowMatchAll}>
-      {error && (
-        <FixedConfirmationToast status="error" text={error} margin="sm" />
-      )}
-      {!context.match_all && (
-        <>
-          {allowIndividuals && (
-            <ScimResourceTypeahead
-              label="Add Individuals"
-              value={context.extra_users || []}
-              onChange={(users: ScimObject[]) => change("extra_users", users)}
-              resourceId={userResource}
-            />
+    <Card margin="xs" padding="xs">
+      <Card.Header headerColor={context.match_all ? "none" : "white"}>
+        <MatchAllToggleHeader allowMatchAll={allowMatchAll} />
+      </Card.Header>
+      <Card.Body>
+        <Flex orientation="column" align="stretch">
+          {error && (
+            <FixedConfirmationToast status="error" text={error} margin="sm" />
           )}
-          <CriteriaList onEditCriteria={setEditing} />
-          <FlexItem alignSelf="center">
-            <Button
-              fixedWidth
-              marginTop="md"
-              onClick={() => setEditing(context.criteria.length)}
-              text={`Add Members by ${toSentence(groupResources)}`}
-              variant="link"
-            />
-          </FlexItem>
-        </>
-      )}
+          {!context.match_all && (
+            <>
+              {allowIndividuals && (
+                <ScimResourceTypeahead
+                  label="Add Individuals"
+                  value={context.extra_users || []}
+                  onChange={(users: ScimObject[]) => change("extra_users", users)}
+                  resourceId={userResource}
+                />
+              )}
+              <CriteriaList onEditCriteria={setEditing} />
+              <FlexItem alignSelf="center">
+                <Button
+                  fixedWidth
+                  marginTop="md"
+                  onClick={() => setEditing(context.criteria.length)}
+                  text={`Add Members by ${toSentence(groupResources)}`}
+                  variant="link"
+                />
+              </FlexItem>
+            </>
+          )}
 
-      <ActionBar />
-    </MatchAllToggleCard>
+          <ActionBar />
+        </Flex>
+      </Card.Body>
+    </Card>
   )
 }
