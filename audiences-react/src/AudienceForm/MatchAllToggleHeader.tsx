a7c31745680cc35ac933dd627b5eeb91e86dbaf2
Kseniia Khodyreva <melidiosone@gmail.com>
2025-10-01 13:39:51 -0400

    [RBLS-2840] fix the bug popoverr close on view members click

--- audiences-react/src/AudienceForm/MatchAllToggleHeader.tsx
+++ audiences-react/src/AudienceForm/MatchAllToggleHeader.tsx
@@ -1,8 +1,9 @@
-import { FlexItem, Toggle } from "playbook-ui"
+import { FlexItem, Toggle, Button } from "playbook-ui"
 import { Caption, Flex } from "playbook-ui"
 
-import { MembersModalButton } from "./MembersModalButton"
+import { MembersModal } from "./MembersModal"
 import { useAudiencesContext } from "../audiences"
+import { useState } from "react"
 
 type MatchAllToggleHeaderProps = {
   allowMatchAll: boolean
@@ -14,40 +15,49 @@ export function MatchAllToggleHeader({
 }: MatchAllToggleHeaderProps) {
   const { value: context, isDirty, change } = useAudiencesContext()
   const handleToggle = () => change("match_all", !context.match_all)
+  const [showMembers, setShowMembers] = useState(false)
 
   return (
-    <Flex orientation={isMobile ? "column" : "row"} spacing="between" wrap>
-      <FlexItem>
-        <Caption text={`Members ${isDirty() ? "" : context.count}`} />
-        {isDirty() ? (
-          <Caption
-            size="xs"
-            marginTop="sm"
-            text="Save changes to view all members"
-          />
-        ) : (
-          <MembersModalButton
-            text="View All"
-            title="All Members"
-            padding="none"
-            total={context.count}
-          />
-        )}
-      </FlexItem>
-      {allowMatchAll && (
+    <>
+      <Flex orientation={isMobile ? "column" : "row"} spacing="between" wrap>
         <FlexItem>
-          <Flex justify="right" orientation="row" align="center">
-            <Toggle>
-              <input
-                type="checkbox"
-                checked={context.match_all}
-                onChange={handleToggle}
-              />
-            </Toggle>
-            <Caption marginLeft="xs" size="xs" text={"All Employees"} />
-          </Flex>
+          <Caption text={`Members ${isDirty() ? "" : context.count}`} />
+          {isDirty() ? (
+            <Caption
+              size="xs"
+              marginTop="sm"
+              text="Save changes to view all members"
+            />
+          ) : (
+            <Button
+              variant="link"
+              padding="none"
+              onClick={() => setShowMembers(true)}
+              text="View All"
+            />
+          )}
         </FlexItem>
-      )}
-    </Flex>
+        {allowMatchAll && (
+          <FlexItem>
+            <Flex justify="right" orientation="row" align="center">
+              <Toggle>
+                <input
+                  type="checkbox"
+                  checked={context.match_all}
+                  onChange={handleToggle}
+                />
+              </Toggle>
+              <Caption marginLeft="xs" size="xs" text={"All Employees"} />
+            </Flex>
+          </FlexItem>
+        )}
+      </Flex>
+      <MembersModal
+        title="All Members"
+        total={context.count}
+        showMembers={showMembers}
+        setShowMembers={setShowMembers}
+      />
+    </>
   )
 }
