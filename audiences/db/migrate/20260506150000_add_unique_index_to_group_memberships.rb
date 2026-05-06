5337c4e515f272ff2b76a014216a0210b681a7ce
Dan Smith <danielgj.smith@gmail.com>
2026-05-06 13:29:51 -0400

    Fix Rubocop offenses

--- audiences/db/migrate/20260506150000_add_unique_index_to_group_memberships.rb
+++ audiences/db/migrate/20260506150000_add_unique_index_to_group_memberships.rb
@@ -3,8 +3,8 @@
 class AddUniqueIndexToGroupMemberships < ActiveRecord::Migration[6.1]
   def change
     add_index :audiences_group_memberships,
-              [:group_id, :external_user_id],
+              %i[group_id external_user_id],
               unique: true,
-              name: 'index_group_memberships_on_group_and_user'
+              name: "index_group_memberships_on_group_and_user"
   end
 end
